5b1dec0020e47619f09b90aa0496bc9bc6f0cfd1
Oleg Strizhechenko <oleg@carbonsoft.ru>
2014-12-22 22:41:04 +0500

    Update install_on_centos.sh

--- install_on_centos.sh
+++ install_on_centos.sh
@@ -17,7 +17,7 @@ mkdir -p /app/base/mnt/{var,log,var/cfg} /var/backup/
 rsync -a --progress -r --port $PORT $HOST::filearchive/profiles/$UPDATE_PRODUCT /tmp/app_list
 for app in auth $(</tmp/app_list); do
 	[ "$app" = 'base' ] && addon='/addon/' || addon=''
-	if [ -f /app/auth/usr/lib/locale/locale-archive ] && [ "$app" != auth ] && [ "$app" != base ]; then # speedup install, -500mb of traffic
+	if [ -f /app/auth/usr/lib/locale/locale-archive ] && [ "$app" != auth ]; then # speedup install, -500mb of traffic
 		mkdir -p /app/$app/usr/lib/locale/ /app/$app/usr/share/locale
 		cp -av /app/{auth,$app}/usr/lib/locale/locale-archive
 		cp -av /app/auth/usr/share/locale/* /app/$app/usr/share/locale/
