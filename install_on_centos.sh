3c7031e3553d30f7d7d83c6a2f6eac07115513f2
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2014-12-26 12:36:06 +0500

    + libxslt

--- install_on_centos.sh
+++ install_on_centos.sh
@@ -34,7 +34,7 @@ rpm -i "http://mirror.yandex.ru/epel/6/i386/epel-release-6-8.noarch.rpm" || true
 sed -e 's/https/http/g' -i /etc/yum.repos.d/epel.repo
 sed -e 's/https/http/g' -i /etc/yum.repos.d/epel-testing.repo
 sed -e 's|Defaults    requiretty|#&|g; s|# %wheel|%wheel|g' -i /etc/sudoers
-yum -y install conntrack-tools mod_wsgi python-markdown dialog git python-suds lsof ntpdate vim mc strace
+yum -y install conntrack-tools mod_wsgi python-markdown dialog git python-suds lsof ntpdate vim mc strace libxslt
 
 echo "# рестартанём все контейнеры один раз чтобы обновление работало"
 for app in base auth $(</tmp/app_list); do
