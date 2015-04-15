b0e3c2c0f81c34490f9592429890737892bca369
Oleg Strizhechenko <panzersoldat@mail.ru>
2015-04-15 16:26:39 +0500

    Update reductor_checker.sh

--- reductor_checker.sh
+++ reductor_checker.sh
@@ -3,24 +3,38 @@
 # works only for reductor on centos
 # for reductor on billing check $? after curl
 
-check_url() {
-	blocked=0
-	for i in {1..100}; do
-		if [ "$(curl -sS $url &>/dev/null)" = '302 Found. Site Block' ]; then
-			((blocked++)) || true
-		fi
-		printf "\b\b\b\b\b\b\b\b\b\b %3d / 100" $i
-	done
-	printf "\n$blocked\n"
+_echo() {
+        if [ "$VERBOSE" = '1' ]; then
+                echo "$@"
+        fi
 }
 
+check_url() {
+        blocked=0
+        for i in {1..100}; do
+                printf "\b\b\b\b\b\b\b\b\b\b %3d / 100" $i
+                if [ "$(curl -sS $url)" = '302 Found. Site Block' ]; then
+                        _echo " blocked"
+                        ((blocked++)) || true
+                else
+                        _echo " opened"
+                fi
+        done
+        printf "\nblocked $blocked times\n"
+}
+
+VERBOSE=0
+if [ "$1" = '-v' ]; then
+        VERBOSE=1
+        shift
+fi
 
 if [ "$#" -gt '0' ]; then
-	for url in $@; do		check_url "$url"
-	done
+        for url in $@; do               check_url "$url"
+        done
 
 else
-	while read -r url tmp; do
-		check_url "$url"
-	done
+        while read -r url tmp; do
+                check_url "$url"
+        done
 fi
