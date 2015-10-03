e4d17c6525dac81d82bae9f28af8570ae1290c31
Oleg Strizhechenko <oleg@carbonsoft.ru>
2015-10-03 12:30:32 +0500

    Update reductor_info.sh

--- reductor_info.sh
+++ reductor_info.sh
@@ -9,7 +9,11 @@ cpu_model() {
 }
 
 get_rx_bytes() {
-	grep $1 /proc/net/dev | tr -d ':' | awk '{print $2}'
+	grep $1 /proc/net/dev | tr ':' ' ' | awk '{print $2}'
+}
+
+get_rx_pkts() {
+	grep $1 /proc/net/dev | tr ':' ' ' | awk '{print $3}'
 }
 
 main() {
@@ -17,18 +21,37 @@ main() {
 		yum -y install pciutils
 	fi
 	clear
-	echo Используемый процессор: $(cpu_count) x $(cpu_model)
-	echo Сетевые карты:
-	echo '{code}'
-	lspci | grep Ethernet | sed -e 's/.*Ethernet controller: //'
-	echo '{code}'
-	echo Объём трафика в зеркале:
+	echo h3. Используемый процессор
+	echo $(cpu_count) x $(cpu_model)
+	echo
+	echo h3. Сетевые карты:
+	lspci | grep Ethernet | sed -e 's/.*Ethernet controller: //' | sort -u | sed 's/^/* /g'
+	echo
+
 	for dev in $(brctl show | grep -o eth[0-9]); do
 		rx1=$(get_rx_bytes $dev)
+		rxp1=$(get_rx_pkts $dev)
 		sleep 1
 		rx2=$(get_rx_bytes $dev)
+		rxp2=$(get_rx_pkts $dev)
 		delta=$((rx2-rx1))
-		echo $dev: ≈$((delta/1024/1024)) mb/sec
+		delta_pkts=$((rxp2-rxp1))
+		if [ $delta = 0 -o $delta_pkts = 0 ]; then
+			continue
+		fi
+		echo "h4. $dev (прерывания/буферы/usecs/объёмы трафика)"
+		echo
+		grep $dev /proc/interrupts
+		echo $(ethtool -g $dev | egrep "RX:|set")
+		ethtool -c $dev | grep "rx-usecs:"
+
+		mirror_dev="$mirror_dev $dev"
+		if [ "$((delta/1024/128)))" != 0 ]; then
+			echo ≈$((delta/1024/128)) mbit/sec $delta_pkts pkts/sec
+		elif [ "$((delta / 128)))" != 0 ]; then
+			echo ≈$((delta/128)) kbit/sec $delta_pkts pkts/sec
+		fi
+		echo
 	done
 }
 
