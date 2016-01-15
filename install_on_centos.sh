7bc74314cae6cf355a62468eec7df34dbb9c5504
e.pimenov <e.pimenov@carbonsoft.ru>
2016-01-15 17:29:54 +0500

    Всегда ставим devel

--- install_on_centos.sh
+++ install_on_centos.sh
@@ -46,7 +46,7 @@ __install() {
 	sed -e 's/https/http/g' -i /etc/yum.repos.d/epel.repo
 	sed -e 's/https/http/g' -i /etc/yum.repos.d/epel-testing.repo
 	sed -e 's|Defaults    requiretty|#&|g; s|# %wheel|%wheel|g' -i /etc/sudoers
-	yum -y install conntrack-tools mod_wsgi python-markdown dialog git python-suds lsof ntpdate vim mc strace libxslt bind-utils python-virtualenv tcpdump m4 ipset
+	yum -y install conntrack-tools mod_wsgi python-markdown dialog git python-suds lsof ntpdate vim mc strace libxslt bind-utils python-virtualenv tcpdump m4 ipset system-config-network-tui
 
 	echo "# рестартанём все контейнеры один раз чтобы обновление работало"
 
@@ -84,6 +84,10 @@ __install() {
 	echo "Установка завершена, осталось только выполнить reboot"
 }
 
+__get_branch_version() {
+	read INSTALL_BRANCH INSTALL_VERSION <<< "$( curl "http://${HOST}:8024/get_version_to_update.php?product=$INSTALL_PRODUCT&cur_branch=$INSTALL_BRANCH")"
+}
+
 __get_update_product() {
 	curl $HOST:8024/products.list > /tmp/products
 	num=1
@@ -116,12 +120,12 @@ main() {
 	__check_inet "$@"
 
 	if [ "$#" != 5 ]; then
-		INSTALL_BRANCH=integra
-		INSTALL_VERSION=cur
+		INSTALL_BRANCH=devel
 		HOST=update51.carbonsoft.ru
+		__get_update_product
+		__get_branch_version
 		PORT=555
 		__check_install_host "$@"
-		__get_update_product
 	else
 		INSTALL_PRODUCT=${1:-Billing}
 		INSTALL_BRANCH=${2:-integra}
